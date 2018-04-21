f6c317abc41e886a619d192d81b984a0e43abc8e
lushantingyue <diyikehan@126.com>
2018-04-21 22:05:42 +0800

    Update supervisor-build.sh

--- supervisor-build.sh
+++ supervisor-build.sh
@@ -1,10 +1,7 @@
 apt-get install supervisor -y \
- && touch /etc/supervisor/conf.d/v2ray.conf \
- && vi /etc/supervisor/conf.d/v2ray.conf \
- && echo -e -n "$V2RAY_CONF" > v2ray.conf \
- && touch /etc/supervisor/conf.d/caddy.conf \
- && vi /etc/supervisor/conf.d/caddy.conf \
- && echo -e -n "$SUPERVISOR_CONF" > caddy.conf \
+ && touch /etc/supervisor/conf.d/v2ray-caddy.conf \
+ && vi /etc/supervisor/conf.d/v2ray-caddy.conf \
+ && echo -e -n "$SUPERVISOR_CONF" > v2ray-caddy.conf \
  && supervisorctl update all \
  && supervisorctl start v2ray \
  && supervisorctl start caddy
