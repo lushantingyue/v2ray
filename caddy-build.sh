2608fecd9b92906f11cee0c95cfb4cba1bba51c2
lushantingyue <diyikehan@126.com>
2018-04-21 23:27:56 +0800

    Update caddy-build.sh

--- caddy-build.sh
+++ caddy-build.sh
@@ -1,7 +1,7 @@
 mkdir /etc/caddy \
- && wget https://github.com/mholt/caddy/releases/download/v0.10.14/caddy_v0.10.14_linux_386.tar.gz \
- && tar -zxvf caddy_v0.10.14_linux_386.tar.gz -C /etc/caddy/caddy \
- && rm -rf caddy_v0.10.14_linux_38 6.tar.gz \
+ && wget https://github.com/mholt/caddy/releases/download/v0.10.14/caddy_v0.10.14_linux_amd64.tar.gz \
+ && tar -zxvf caddy_v0.10.14_linux_amd64.tar.gz -C /etc/caddy/caddy \
+ && rm -rf caddy_v0.10.14_linux_amd64.tar.gz \
  && chmod +x /etc/caddy/caddy \
  && touch /etc/caddy/caddy.conf \
  && vi /etc/caddy/caddy.conf \
