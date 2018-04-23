7fb74b88a4faff851153c2dc6ec7757e8381578e
lushantingyue <diyikehan@126.com>
2018-04-23 15:03:48 +0800

    Update supervisor-build.sh

--- supervisor-build.sh
+++ supervisor-build.sh
@@ -1,7 +1,9 @@
 apk add supervisor \
+ && mkdir /etc/supervisor/conf.d \
  && touch /etc/supervisor/conf.d/v2ray-caddy.conf \
  && cd    /etc/supervisor/conf.d \
  && echo -e -n "$SUPERVISOR_CONF" > v2ray-caddy.conf \
+ && supervisord -c /etc/supervisor/conf.d/v2ray-caddy.conf
  && supervisorctl update all \
  && supervisorctl restart v2ray \
  && supervisorctl start caddy
