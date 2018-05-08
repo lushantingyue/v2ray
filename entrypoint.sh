2d338fe53e4111bcb1cc19c6b107dcf949ef2537
lushantingyue <diyikehan@126.com>
2018-05-08 16:43:06 +0800

    fix:config.json generate problem

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
-cd /etc/v2ray
-echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$PORT" >> config.json
-echo -e -n "$CONFIG_JSON2" >> config.json
-echo -e -n "$UUID" >> config.json
-echo -e -n "$CONFIG_JSON3" >> config.json
-if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-cd /etc/v2ray
-echo -e "$CERT_PEM" > cert.pem
-echo -e "$KEY_PEM"  > key.pem
+cd /v2raybin \
+echo -e -n "$CONFIG_JSON1 \c" > config.json \
+echo -e -n "$PORT \c" >> config.json \
+echo -e -n "$CONFIG_JSON2 \c" >> config.json \
+echo -e -n "$UUID \c" >> config.json \
+echo -e -n "$CONFIG_JSON3" >> config.json \
+if [ "$CERT_PEM" != "$KEY_PEM" ]; then \
+ cd /v2raybin \
+ echo -e "$CERT_PEM" > cert.pem \
+ echo -e "$KEY_PEM"  > key.pem \
 fi
