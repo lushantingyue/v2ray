b863b8d220f1374bb7ded0b8d951decb05d8b708
lushantingyue <diyikehan@126.com>
2018-05-08 17:24:39 +0800

    Update entrypoint.sh
    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@ echo -e -n "$CONFIG_JSON1 \c" > config.json \
 echo -e -n "$PORT \c" >> config.json \
 echo -e -n "$CONFIG_JSON2 \c" >> config.json \
 echo -e -n "$UUID \c" >> config.json \
-echo -e -n "$CONFIG_JSON3" >> config.json \
-if [ "$CERT_PEM" != "$KEY_PEM" ]; then 
- cd /v2raybin \
- echo -e "$CERT_PEM" > cert.pem \
- echo -e "$KEY_PEM"  > key.pem 
-fi
+echo -e -n "$CONFIG_JSON3" >> config.json 
+#if [ "$CERT_PEM" != "$KEY_PEM" ]; then 
+# cd /etc/v2ray \
+# echo -e "$CERT_PEM" > cert.pem \
+# echo -e "$KEY_PEM"  > key.pem 
+#fi
