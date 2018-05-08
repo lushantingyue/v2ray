d1e768dfdd670149a6e90df179f1afeef7e9d159
lushantingyue <diyikehan@126.com>
2018-05-08 17:41:55 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
-cd /etc/v2ray \
-echo -e -n "$CONFIG_JSON1 \c" > config.json \
-echo -e -n "$PORT \c" >> config.json \
-echo -e -n "$CONFIG_JSON2 \c" >> config.json \
-echo -e -n "$UUID \c" >> config.json \
+cd /etc/v2ray && \
+echo -e -n "$CONFIG_JSON1 \c" > config.json && \
+echo -e -n "$PORT \c" >> config.json && \
+echo -e -n "$CONFIG_JSON2 \c" >> config.json && \
+echo -e -n "$UUID \c" >> config.json && \
 echo -e -n "$CONFIG_JSON3" >> config.json 
 #if [ "$CERT_PEM" != "$KEY_PEM" ]; then 
 # cd /etc/v2ray \
