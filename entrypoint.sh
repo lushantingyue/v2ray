62386ed400146b8a66d6f928ceaa79cec50461bb
lushantingyue <diyikehan@126.com>
2018-05-09 20:23:59 +0800

    Update entrypoint.sh
    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
-cd /etc/v2ray
-echo -e -n "$CONFIG_JSON1 \c" > config.json
-echo -e -n "$PORT \c" >> config.json
-echo -e -n "$CONFIG_JSON2 \c" >> config.json
-echo -e -n "$UUID \c" >> config.json
-echo -e -n "$CONFIG_JSON3" >> config.json
+# cd /etc/v2ray
+# echo -e -n "$CONFIG_JSON1 \c" > config.json
+# echo -e -n "$PORT \c" >> config.json
+# echo -e -n "$CONFIG_JSON2 \c" >> config.json
+# echo -e -n "$UUID \c" >> config.json
+# echo -e -n "$CONFIG_JSON3" >> config.json
+
 #if [ "$CERT_PEM" != "$KEY_PEM" ]; then 
 # cd /etc/v2ray \
 # echo -e "$CERT_PEM" > cert.pem \
 # echo -e "$KEY_PEM"  > key.pem 
 #fi
-/usr/bin/supervisord -c /etc/supervisord.conf && supervisorctl update && supervisorctl restart all
+
+# /usr/bin/supervisord -c /etc/supervisord.conf && supervisorctl update && supervisorctl restart all
+supervisord -c /etc/supervisord.conf && supervisorctl status && supervisorctl restart v2ray
