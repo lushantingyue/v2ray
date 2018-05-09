1ee7533cb4dba724f77d4c6c00be9396c004f1cc
lushantingyue <diyikehan@126.com>
2018-05-09 20:53:57 +0800

    Update entrypoint.sh
    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,5 @@
 #fi
 
 # /usr/bin/supervisord -c /etc/supervisord.conf && supervisorctl update && supervisorctl restart all
-./supervisord -c /etc/supervisord.conf && ./supervisorctl status && ./supervisorctl restart v2ray
+# ./supervisord -c /etc/supervisord.conf && ./supervisorctl status && ./supervisorctl restart v2ray
+./v2ray -config=/etc/v2ray/config/json
