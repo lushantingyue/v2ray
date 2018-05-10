f105a8db1f3fa38920205f3629786113a7617c01
lushantingyue <diyikehan@126.com>
2018-05-10 15:07:47 +0800

    fix:shell脚本语法错误, 路径操作放到docker run里面

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
-cd /etc/v2ray \
- && echo -e -n "$CONFIG_JSON1"   >  config.json \
- && echo -e -n "$PORT"           >> config.json \
- && echo -e -n "$CONFIG_JSON2"   >> config.json \
- && echo -e -n "$UUID"           >> config.json \
- && echo -e -n "$CONFIG_JSON3"   >> config.json
-
+echo -e -n "$CONFIG_JSON1"   >  config.json
+echo -e -n "$PORT"           >> config.json
+echo -e -n "$CONFIG_JSON2"   >> config.json
+echo -e -n "$UUID"           >> config.json
+echo -e -n "$CONFIG_JSON3"   >> config.json
 # /usr/bin/supervisord -c /etc/supervisord.conf && supervisorctl update && supervisorctl restart all
 # ./supervisord -c /etc/supervisord.conf && ./supervisorctl status && ./supervisorctl restart v2ray
 # ./v2ray -config="/etc/v2ray/config.json"
