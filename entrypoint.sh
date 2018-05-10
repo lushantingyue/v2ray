55c719f809635d62701017188c5da4638af2dffd
lushantingyue <diyikehan@126.com>
2018-05-11 01:13:18 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 echo -e -n "$CONFIG_JSON1"   >  /etc/v2ray/config.json
-echo -e -n "$PORT"           >> /etc/v2ray/config.json
+echo -e -n "$PORT01"           >> /etc/v2ray/config.json
 echo -e -n "$CONFIG_JSON2"   >> /etc/v2ray/config.json
 echo -e -n "$UUID"           >> /etc/v2ray/config.json
 echo -e -n "$CONFIG_JSON3"   >> /etc/v2ray/config.json
