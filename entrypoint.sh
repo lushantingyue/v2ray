7b4de16c909a636ed5b295d7f0239d699159e683
lushantingyue <diyikehan@126.com>
2018-05-10 17:11:38 +0800

    Update entrypoint.sh
    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
-echo -e -n "$CONFIG_JSON1"   >  /etc/v2ray/config.json
-echo -e -n "$PORT"           >> /etc/v2ray/config.json
-echo -e -n "$CONFIG_JSON2"   >> /etc/v2ray/config.json
-echo -e -n "$UUID"           >> /etc/v2ray/config.json
-echo -e -n "$CONFIG_JSON3"   >> /etc/v2ray/config.json
+cd /etc/v2ray/ || echo pwd > err.log
+echo -e -n "$CONFIG_JSON1"   >  config.json
+echo -e -n "$PORT"           >> config.json
+echo -e -n "$CONFIG_JSON2"   >> config.json
+echo -e -n "$UUID"           >> config.json
+echo -e -n "$CONFIG_JSON3"   >> config.json
 # /usr/bin/supervisord -c /etc/supervisord.conf && supervisorctl update && supervisorctl restart all
 # ./supervisord -c /etc/supervisord.conf && ./supervisorctl status && ./supervisorctl restart v2ray
 # ./v2ray -config="/etc/v2ray/config.json"
